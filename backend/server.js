00be22314a83a42639c6202971b3044f53c14796
HEVEN-STORES <heven.storess@gmail.com>
2026-01-03 10:39:27 +0530

    admin cors fixed

--- backend/server.js
+++ backend/server.js
@@ -356,7 +356,8 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5174',
   'http://localhost:4173',
-  'https://tedx-smec-website.vercel.app'
+  'https://tedx-smec-website.vercel.app',
+   'https://tedx-smec-admin.vercel.app'
 ].filter(Boolean);
 
 const corsOptions = {
